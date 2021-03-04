3cb8435bbd865e93db6d455de609c4d101f61017
Joan Andrés <andres6936@live.com>
2021-03-04 12:45:01 -0500

    Simplify get the file from resources folder

--- src/main/java/man/app/route/ShowHandle.java
+++ src/main/java/man/app/route/ShowHandle.java
@@ -4,24 +4,17 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
+import java.nio.file.Files;
 
 public class ShowHandle implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if (exchange.getRequestMethod().equals("GET")) {
-            File page = new File(Objects.requireNonNull(
-                    HomeHandler.class.getClassLoader().getResource("html/show.html")).getFile());
-            FileInputStream inputStream = new FileInputStream(page);
-            byte[] inputData = new byte[(int) page.length()];
-            inputStream.read(inputData);
-            inputStream.close();
+            File page = new File(ClassLoader.getSystemResource("html/show.html").getFile());
 
-            String contentPage = new String(inputData, StandardCharsets.UTF_8);
+            String contentPage = new String(Files.readAllBytes(page.toPath()));
 
             exchange.sendResponseHeaders(200, contentPage.length());
 
