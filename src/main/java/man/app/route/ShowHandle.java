94c13c3ab1efe7319477026bc53f5000da86e3a9
Joan Andrés <andres6936@live.com>
2021-03-04 17:34:46 -0500

    Connection to MySQL 8.0.23

--- src/main/java/man/app/route/ShowHandle.java
+++ src/main/java/man/app/route/ShowHandle.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.sql.*;
 
 public class ShowHandle implements HttpHandler {
     @Override
@@ -22,6 +23,26 @@ public class ShowHandle implements HttpHandler {
             outputStream.write(contentPage.getBytes());
             outputStream.flush();
             outputStream.close();
+        }
+
+        selectAll();
+    }
+
+    private void selectAll() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Mandomedia", "root", "HDgtDVi5");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * From Employee");
+            while (resultSet.next()) {
+                System.out.println("DUI: " + resultSet.getInt(1));
+            }
+            connection.close();
+        } catch (SQLException ignored) {
+            System.err.println("Error MySQL Connection");
+        } catch (ClassNotFoundException ignored) {
+            System.err.println("The Driver hasn't been loaded");
         }
     }
 }
