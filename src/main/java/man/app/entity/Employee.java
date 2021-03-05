f9c064c6c72a3733491cf9d373b7651e4d9c401a
Joan Andrés <andres6936@live.com>
2021-03-04 21:48:01 -0500

    Added Entity

---
+++ src/main/java/man/app/entity/Employee.java
@@ -0,0 +1,71 @@
+package man.app.entity;
+
+public class Employee {
+
+    // Properties
+
+    private int dui;
+    private int nit;
+    private float salary;
+    private String name;
+    private String department;
+    private MaritalStatus maritalStatus = MaritalStatus.Free;
+
+    // Constructor
+
+    public Employee(int _dui, String _name, float _salary) {
+        this.dui = _dui;
+        this.name = _name;
+        this.salary = _salary;
+    }
+
+    // Getters and Setters
+
+    public int getDui() {
+        return dui;
+    }
+
+    public void setDui(int dui) {
+        this.dui = dui;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public MaritalStatus getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(MaritalStatus maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+}
---
+++ src/main/java/man/app/entity/MaritalStatus.java
@@ -0,0 +1,9 @@
+package man.app.entity;
+
+public enum MaritalStatus {
+    Free,
+    Married,
+    Widowed,
+    Separated,
+    Unmarried
+}
