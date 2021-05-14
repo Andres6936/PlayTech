84073c0cace22f15de484c2334eaa6796849978f
Joan Andrés <andres6936@live.com>
2021-05-13 22:12:57 -0500

    Implements HttpHandler

--- src/main/java/man/app/route/WaiterUpdateHandle.java
+++ src/main/java/man/app/route/WaiterUpdateHandle.java
@@ -1,4 +1,27 @@
 package man.app.route;
 
-public class WaiterUpdateHandle {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class WaiterUpdateHandle implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if (exchange.getRequestMethod().equals("GET")) {
+            File page = new File(ClassLoader.getSystemResource("html/show.html").getFile());
+
+            String contentPage = new String(Files.readAllBytes(page.toPath()));
+
+            exchange.sendResponseHeaders(200, contentPage.length());
+
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(contentPage.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        }
+    }
 }
