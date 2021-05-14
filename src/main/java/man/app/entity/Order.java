dc38948f89b854c783ec44a249a05e7fb999ec6a
Joan Andrés <andres6936@live.com>
2021-05-13 22:06:30 -0500

    Added the entities

---
+++ src/main/java/man/app/entity/Order.java
@@ -0,0 +1,9 @@
+package man.app.entity;
+
+public class Order {
+    public int identification;
+    public int price;
+    public String starter;
+    public String entree;
+    public String appetizer;
+}
---
+++ src/main/java/man/app/entity/Waiter.java
@@ -0,0 +1,8 @@
+package man.app.entity;
+
+public class Waiter {
+    public int identification;
+    public int table;
+    public String firstName;
+    public String lastName;
+}
