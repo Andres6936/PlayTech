2105613204655572f79bad079e0caf2fda94977d
Joan Andrés <andres6936@live.com>
2021-05-16 07:10:40 -0500

    Added the Waiter All

--- src/main/java/man/app/entity/Waiter.java
+++ src/main/java/man/app/entity/Waiter.java
@@ -5,4 +5,15 @@ public class Waiter {
     public int table;
     public String firstName;
     public String lastName;
+
+    public Waiter(int identification, String firstName, String lastName) {
+        this.identification = identification;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getJSON() {
+        return String.format("{\"identification\":%d, \"firstName\":\"%s\", \"lastName\":\"%s\"},",
+                identification, firstName, lastName);
+    }
 }
